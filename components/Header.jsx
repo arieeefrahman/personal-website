5e223cd99ea9eb73a411646d2f8df2ae7ae544c3
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-08-12 00:55:29 +0700

    feat: header

---
+++ components/Header.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Navbar from "./Navbar";
+import { Button } from "./ui/button";
+
+const Header = () => {
+    return (
+        <header className="py-8 xl:py-12 text-white">
+            <div className="container mx-auto flex justify-between items-center">
+                {/* Logo */}
+                <Link href={'/'}>
+                    <h1 className="text-4xl font-semibold">
+                        Arief <span className="text-accent">.</span>
+                    </h1>
+                </Link>
+
+                {/* Navigation bar : desktop */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Navbar />
+                    <Link href={'/contact'}>
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/* Navigation bar : mobile */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
