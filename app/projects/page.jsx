5b6ea050b1709aea3b87b55b69ee9f67da978706
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-08-18 02:02:16 +0700

    feat: add animation in under development page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div className='container mx-auto'>Under Development...</div>
+    <>
+		<div class="area">
+			<ul class="circles">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+      <div className="container h3 xl:h2 font-bold uppercase z-10 py-[220px] flex justify-center items-center my-auto">Under Development...</div>
+		</div>
+    </>
   )
 }
 
