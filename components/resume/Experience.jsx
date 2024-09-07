510e420d45679419dff1bb703c023ec6da4db1a4
arieeefrahman <ariefrahman01@mail.ugm.ac.id>
2024-09-07 10:33:23 +0700

    feat: add new experiences

--- components/resume/Experience.jsx
+++ components/resume/Experience.jsx
@@ -5,10 +5,28 @@ const experiences = {
     description: 'Overview of my experiences and roles held.',
     items: [
         {
+            company: 'cmlabs',
+            position: 'Backend Developer Intern',
+            duration: 'Aug 2024 - Present',
+            url: 'https://cmlabs.co/en-id',
+            place: 'Malang, East Java · (Remote)',
+            responsibilities: [
+                'Perform application optimization for maximum speed and scalability.',
+                'Implement clean and secure code.',
+                'Provide effective and efficient suggestions and solutions to the team.',
+                'Solve problem such as bugs or errors related to the project being worked on.',
+                'Coordinate with the whole team.',
+                'Design and implement data storage solutions.',
+                'Doing API Integration.',
+                'Perform security settings and prevent hacking attacks.',
+            ],
+        },
+        {
             company: 'BINAR Academy',
             position: 'Backend Engineer Intern',
             duration: 'Aug 2023 - Dec 2023',
             url: 'https://www.binaracademy.com/',
+            place: 'Tangerang, Banten · On-site',
             responsibilities: [
                 'Designed and implemented microservices using Golang and Ruby on Rails with REST APIs and gRPC.',
                 'Developed and maintained unit tests for Go (Testify) and Ruby (RSpec) to guarantee code quality and functionality',
@@ -33,18 +51,25 @@ const Experience = () => (
                         key={idx}
                         className='bg-[#232329] py-6 px-10 rounded-xl flex flex-col gap-1'
                     >
-                        <span className='text-accent text-left'>{item.duration}</span>
-                        <h3 className='text-xl max-w-[360px] min-h-[30px] text-left'>{item.position}</h3>
-                        <div className='flex gap-3'>
+                        <div className="flex justify-between flex-col md:flex-row text-left">
+                            <span className='text-accent text-left order-2'>{item.duration}</span>
+                            <h3 className='text-xl max-w-[360px] min-h-[30px] order-1'>{item.position}</h3>
+                        </div>
+                        
+                        
+                        <div className='flex justify-between flex-col md:flex-row text-left'>
                             {/* <span className='w-[6px] h-[6px] rounded-full bg-accent'></span> */}
                             <a href={item.url} target='_blank' className='text-white/60'>{item.company}</a>
+                            <p className='text-white/60'>{item.place}</p>
+
                         </div>
 
-                        <p className='pt-5 font-bold text-left'>Responsibilities : </p>
+
+                        <p className='pt-5 font-bold text-left'>Responsibilities: </p>
                         <ul className='list-disc ml-[15px] text-left'>
                             {item.responsibilities.map((detail, detailIdx) => (
                                 <li key={detailIdx} className='text-white/60'>
-                                <p className='ml-1 max-w-[400px] xl:max-w-[600px] text-justify text-white/60'>{detail}</p>
+                                <p className='ml-1 max-w-[500px] md:max-w-[1070px] xl:max-w-[650px] text-justify text-white/60'>{detail}</p>
                                 </li>
                             ))}
                         </ul>
